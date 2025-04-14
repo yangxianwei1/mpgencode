b9d3fdad4e96e09fb266382421d89d533a7dea38
yangxianwei1 <41825804+yangxianwei1@users.noreply.github.com>
2025-04-14 10:47:59 +0800

    Update Main.java
    MP模板代码

--- Main.java
+++ Main.java
@@ -13,7 +13,7 @@ import com.baomidou.mybatisplus.generator.config.rules.NamingStrategy;
 import lombok.extern.slf4j.Slf4j;
 
 import java.util.Scanner;
-
+ 
 @Slf4j
 public class Main {
     public static String scanner(String tip) {
